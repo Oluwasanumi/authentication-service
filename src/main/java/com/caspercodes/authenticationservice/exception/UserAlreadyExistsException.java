8511b5ebaf8140648d6006f68470926fe8a2b84c
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-05-29 22:33:26 +0100

    Fixing errors in exception classes

--- src/main/java/com/caspercodes/authenticationservice/exception/UserAlreadyExistsException.java
+++ src/main/java/com/caspercodes/authenticationservice/exception/UserAlreadyExistsException.java
@@ -1,9 +1,9 @@
 package com.caspercodes.authenticationservice.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus; /**
- * Thrown when user already exists
- */
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
 @ResponseStatus(HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends AuthenticationException {
     public UserAlreadyExistsException(String email) {
