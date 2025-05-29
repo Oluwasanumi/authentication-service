830621cf5867fd37b43ee7e94f4ed64e2803e4eb
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-05-29 22:33:51 +0100

    Fixing errors in exception classes

--- src/main/java/com/caspercodes/authenticationservice/exception/InvalidTokenException.java
+++ src/main/java/com/caspercodes/authenticationservice/exception/InvalidTokenException.java
@@ -1,9 +1,9 @@
 package com.caspercodes.authenticationservice.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus; /**
- * Thrown when JWT token is invalid
- */
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidTokenException extends AuthenticationException {
     public InvalidTokenException(String message) {
