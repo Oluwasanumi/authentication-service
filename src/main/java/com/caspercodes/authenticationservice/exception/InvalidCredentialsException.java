3bd1b16c81cf2d155eea25c27804f59815f93269
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-05-29 22:34:28 +0100

    Editing exception classes

--- src/main/java/com/caspercodes/authenticationservice/exception/InvalidCredentialsException.java
+++ src/main/java/com/caspercodes/authenticationservice/exception/InvalidCredentialsException.java
@@ -1,9 +1,9 @@
 package com.caspercodes.authenticationservice.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus; /**
- * Thrown when credentials are invalid
- */
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidCredentialsException extends AuthenticationException {
     public InvalidCredentialsException() {
