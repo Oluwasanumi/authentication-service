ede35b046d64f322a489217e1201b20ceef68971
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-05-29 22:34:13 +0100

    Editing exception classes

--- src/main/java/com/caspercodes/authenticationservice/exception/InvalidOtpException.java
+++ src/main/java/com/caspercodes/authenticationservice/exception/InvalidOtpException.java
@@ -1,9 +1,9 @@
 package com.caspercodes.authenticationservice.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus; /**
- * Thrown when OTP is invalid or expired
- */
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidOtpException extends AuthenticationException {
     public InvalidOtpException() {
