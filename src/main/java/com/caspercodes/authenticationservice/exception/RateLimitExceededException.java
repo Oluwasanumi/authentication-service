b1122d59b20099c4fc74e0c0897e3e42d14983ba
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-05-29 22:33:38 +0100

    Fixing errors in exception classes

--- src/main/java/com/caspercodes/authenticationservice/exception/RateLimitExceededException.java
+++ src/main/java/com/caspercodes/authenticationservice/exception/RateLimitExceededException.java
@@ -1,9 +1,9 @@
 package com.caspercodes.authenticationservice.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus; /**
- * Thrown when rate limit is exceeded
- */
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
 @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
 public class RateLimitExceededException extends AuthenticationException {
     public RateLimitExceededException(String message) {
